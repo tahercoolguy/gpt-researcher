2166551c381863b7fa377e6f1e430b78860cab29
Taher Ali badnawarwala <tahercoolguy@gmail.com>
2023-12-04 09:19:51 +0530

    Removed Serpe API

--- gpt_researcher/retrievers/__init__.py
+++ gpt_researcher/retrievers/__init__.py
@@ -2,7 +2,7 @@ from .tavily_search.tavily_search import TavilySearch
 from .duckduckgo.duckduckgo import Duckduckgo
 from .google.google import GoogleSearch
 from .serper.serper import SerperSearch
-from .serpapi.serpapi import SerpApiSearch
+# from .serpapi.serpapi import SerpApiSearch
 from .searx.searx import SearxSearch
 
-__all__ = ["TavilySearch", "Duckduckgo", "SerperSearch", "SerpApiSearch", "GoogleSearch", "SearxSearch"]
+__all__ = ["TavilySearch", "Duckduckgo", "SerperSearch", "GoogleSearch", "SearxSearch"] #, "SerpApiSearch"
