f8f0a34f717f3623ece5e1f513fb09aedb8816cc
Taher Ali badnawarwala <tahercoolguy@gmail.com>
2024-01-04 11:55:30 +0530

    embedding Added

--- gpt_researcher/memory/embeddings.py
+++ gpt_researcher/memory/embeddings.py
@@ -1,10 +1,13 @@
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass("Provide your Google API key here")
 
 class Memory:
     def __init__(self, **kwargs):
-        self._embeddings = OpenAIEmbeddings()
+        self._embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001") #OpenAIEmbeddings()
 
     def get_embeddings(self):
         return self._embeddings
